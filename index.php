790ac236cecd84cbed5573c38928cba2b0d1bc66
amine7777777chabane <aminechabane7@hotmail.com>
2025-03-06 14:20:58 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Ma première application PHP sur Azure</title>
-</head>
-<body>
-    <h1><?php echo "Bonjour, monde !"; ?></h1>
-    <p>Vous êtes sur un serveur utilisant PHP version : <?php echo phpversion(); ?></p>
-    <p>Date et heure actuelles : <?php echo date("d/m/Y H:i:s"); ?></p>
-</body>
+    <head>
+        <title>Test PHP</title>
+    </head>
+    <body>
+        <?php echo '<p>Bonjour le monde</p>'; ?>
+    </body>
 </html>
-
