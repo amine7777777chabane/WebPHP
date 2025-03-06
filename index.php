aff9cd0bd9c2ce91a803e4347e4a6c710ab43ae2
amine7777777chabane <aminechabane7@hotmail.com>
2025-03-06 14:18:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Exemple PHP</title>
+    <title>Ma première application PHP sur Azure</title>
 </head>
 <body>
     <h1><?php echo "Bonjour, monde !"; ?></h1>
-    <p>Nous sommes le : <?php echo date("d/m/Y"); ?></p>
+    <p>Vous êtes sur un serveur utilisant PHP version : <?php echo phpversion(); ?></p>
+    <p>Date et heure actuelles : <?php echo date("d/m/Y H:i:s"); ?></p>
 </body>
 </html>
 
