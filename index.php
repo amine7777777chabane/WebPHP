e3e0f0a45e0bd3cdbba753d5d8a8a778dd66173b
amine7777777chabane <aminechabane7@hotmail.com>
2025-03-06 14:10:15 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Exemple PHP</title>
+</head>
+<body>
+    <h1><?php echo "Bonjour, monde !"; ?></h1>
+    <p>Nous sommes le : <?php echo date("d/m/Y"); ?></p>
+</body>
+</html>
 
