2a537a77f419e05e04b8d5989d5698fef602a0b8
amine7777777chabane <aminechabane7@hotmail.com>
2025-03-06 14:44:28 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
         <title>Test PHP</title>
     </head>
     <body>
-        <?php echo '<p>Bonjour le monde</p>'; ?>
+        <?php echo '<p>Bonjour ma vie et vous mes desespoir , me revoici au faossé ou naquit ma misere , voici le coin de boue ou dormais mon front fiere voici ma vie a moi rassembler en poussieres</p>'; ?>
     </body>
 </html>
